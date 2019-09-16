814bca058f75b5372b9ca3e31c2f79f3d88b3927
xalkan <shahidalyk@gmail.com>
2019-09-17 00:17:46 +0500

    project setup

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Starting a new project</h1>
+            <p>Thats right, starting a new project</p>
+        </div>
+    )
+}
+
+export default IndexPage
