7cd6de94ad19399d10560eaac60de700d29a3884
xalkan <shahidalyk@gmail.com>
2019-09-19 01:58:32 +0500

    css module for header and nav list

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import headerstyles from './header.module.scss'
+
 const Header = () => {
     return (
-        <header>
-            <h1>Xalkan</h1>
+        <header className={headerstyles.header}>
+            <h1>
+                <Link to="/" className={headerstyles.title}>
+                    Xalkan
+                </Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerstyles.navlist}>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="/" className={headerstyles.navitem} activeClassName={headerstyles.activeNavItem}>Home</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link to="/about" className={headerstyles.navitem} activeClassName={headerstyles.activeNavItem}>About</Link>
                     </li>
                     <li>
-                        <Link to="/blog">Blog</Link>
+                        <Link to="/blog" className={headerstyles.navitem} activeNavItem={headerstyles.activeNavItem}>Blog</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link to="/contact" className={headerstyles.navitem} activeClassName={headerstyles.activeNavItem}>Contact</Link>
                     </li>
                 </ul>
             </nav>
