b1c8107915c726d2f22a06ba88baaa1ff20b2742
xalkan <shahidalyk@gmail.com>
2019-09-25 23:18:00 +0500

    css updates with blog posts restructure

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import blogStyles from './blog.module.scss'
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -26,11 +27,11 @@ const BlogPage = () => {
         <div>
             <Layout>
                 <h1>Blog</h1>
-                <ol>
+                <ol className={blogStyles.posts}>
                     { 
                         data.allMarkdownRemark.edges.map((edge) => {
                             return (
-                                <li>
+                                <li className={blogStyles.post}>
                                     <Link to={`/blog/${edge.node.fields.slug}`} >
                                         <h2>
                                             {edge.node.frontmatter.title}
